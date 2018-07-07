1fc1b10feec004c40cab4c4a7223defaca1856cb
panigrahy.vivek.tmp@gmail.com <vivekananda.panigrahy@sap.com>
2018-07-06 23:57:23 -0400

    Basic Web app
    Change-Id: Iae9e4423868c7db4b0bd60071c7cbb03acc2b36d

--- src/main/java/com/vivek/springlearnings/SpringlearningApplication.java
+++ src/main/java/com/vivek/springlearnings/SpringlearningApplication.java
@@ -2,6 +2,10 @@ package com.vivek.springlearnings;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringlearningApplication {
@@ -9,4 +13,23 @@ public class SpringlearningApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringlearningApplication.class, args);
 	}
+	
+	/*@RestController
+    @RequestMapping("/")
+    class HelloController {
+        @GetMapping
+        public String getMessage() {
+            return "Hello World!!";
+        }
+    }*/
+	
+	
+	@RestController
+    @RequestMapping("hello")
+    class HelloControllerWithParams {
+        @GetMapping
+        public String getMessage(@RequestParam(defaultValue = "Hugo") String name) {
+            return "Hello " + name;
+        }
+    }
 }
