144eb58f0d9910777226d45507975a38705ec01e
panigrahy.vivek.tmp@gmail.com <vivekananda.panigrahy@sap.com>
2018-07-06 23:44:52 -0400

    Initial Project

---
+++ src/main/java/com/vivek/springlearnings/SpringlearningApplication.java
@@ -0,0 +1,12 @@
+package com.vivek.springlearnings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlearningApplication.class, args);
+	}
+}
